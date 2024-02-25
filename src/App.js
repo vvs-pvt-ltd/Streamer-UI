5d85001bc77d98a8712e6d7a17c58422e289622e
Vivek Sahu <viveksahu1762@gmail.com>
2024-02-25 21:47:37 +0530

    Stream

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, Login } from "./pages";
+import { Navbar, Sidebar } from "./components";
+import Demo from "./components/Demo";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-[250px]">
+          <Sidebar />
+        </div>
+        <div className="w-full md:w-auto">
+          <Routes>
+            <Route path="/" element={<Demo />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = () => {
+  return <div className="max-w-[100px]">Sidebar</div>;
+};
+
+export default Sidebar;
