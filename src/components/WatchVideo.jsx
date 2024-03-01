f2a0d7fa0e368cbc72feb7020d323938ac8df08b
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-02 03:13:45 +0530

    Done

--- src/components/WatchVideo.jsx
+++ src/components/WatchVideo.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-
 const socket = io.connect('http://localhost:9000/');
+
 const WatchVideo = () => {
   const videoRef = useRef();
 
-  socket.on("View", (view) => {
-    console.log("Connected to server", view);
-    videoRef.current.src = view;
-  });
+  useEffect(() => {
+    const handleStream = (stream) => {
+      if (videoRef.current) {
+        const blob = new Blob([stream], { type: 'video/mp4' });
+        const url = URL.createObjectURL(blob);
+        videoRef.current.src = url;
+        
+        videoRef.current.play().catch(error => console.error('Autoplay error:', error));
+      }
+    };
 
- 
+    socket.on("View", handleStream);
+
+    return () => {
+      socket.off("View", handleStream);
+    };
+  }, []); 
 
   return (
     <div>
       <p>Video will appear below when it's available</p>
-      <video ref={videoRef} autoPlay></video>
+      <video ref={videoRef} autoPlay controls></video>
     </div>
   );
 };
